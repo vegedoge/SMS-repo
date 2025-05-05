9a26dd4040c7217600431f7e2b702df792d68190
KyonQi <kyon942755317@hotmail.com>
2025-05-05 15:40:51 +0200

    Finish testing the prediction function in KNN

--- app/src/androidTest/java/com/example/ss_lab1/KNNClassifierTest.java
+++ app/src/androidTest/java/com/example/ss_lab1/KNNClassifierTest.java
@@ -16,6 +16,25 @@ public class KNNClassifierTest {
     public void setup() throws Exception {
         Context context = ApplicationProvider.getApplicationContext();
         knn = new KNNClassifier(context, "acc_train_model.json", 3);
+        knn.printTrainSet();
+    }
+
+    @Test
+    public void testPredict() {
+        float[] rawInputStill1 = new float[] {-0.20930018f,	2.2678692f,	9.267372f, 0.392722f, 0.35653067f, 0.332901f, 9.544078f};
+        float[] rawInputStill2 = new float[] {0.044123646f,	2.4320655f,	9.299658f,	0.13878371f,	0.16031909f,	0.6248255f,	9.612972f};
+        float[] rawInputMove1 = new float[] {-0.5685346f,	3.4986897f,	8.767387f,	0.8117651f,	0.7663014f,	2.81067f,	9.4634f};
+        float[] rawInputMove2 = new float[] {0.3226243f,	2.715357f,	10.536249f,	0.689432f,	1.2281163f,	2.2384853f,	10.898175f};
+        String label1 = knn.predict(rawInputStill1);
+        String label2 = knn.predict(rawInputStill2);
+        String label3 = knn.predict(rawInputMove1);
+        String label4 = knn.predict(rawInputMove2);
+
+        Assert.assertEquals("Still", label1);
+        Assert.assertEquals("Still", label2);
+        Assert.assertEquals("Walking", label3);
+        Assert.assertEquals("Walking", label4);
+//        assertEquals("walking", label);
     }
 
 //    @Test
